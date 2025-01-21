27602e30d3f57f082ad52c4269a2d0fcbd6d9236
Thiago Carvalho <thiagoc413@gmail.com>
2025-01-20 21:03:44 -0300

    atualiza config.py

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config():
-    DATABASE_URL = os.getenv('DATABASE_URL')
+
+    DATABASE_URL = os.getenv('DATABASE_URL1')
     BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 
